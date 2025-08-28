27b2a30571eed4adbf15fc8f9fa74bb10d482176
Rafael Borges <rafaborgesdasilva75@gmail.com>
2025-08-28 20:30:54 -0300

    Delete exemplo2.php

--- exemplo2.php
+++
@@ -1,8 +0,0 @@
-<?php
-    // primeira aula de PHP
-    echo "Hello World!"; // echo = print em python
-    $numero_produtos = 0;
-    if ($numero_produtos == 0){
-        echo "local host meu piru";
-    }
-?>
