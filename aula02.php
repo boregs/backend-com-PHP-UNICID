10b704aa90d2e8e6649477703a5b674ee3a7c0f7
RAFAEL BORGES DA SILVA <rafaelsilva007@cs.unicid.edu.br>
2025-09-01 19:51:52 -0300

    Aula 2 de PHP

---
+++ aula02.php
@@ -0,0 +1,11 @@
+<?php 
+    $var = 'texto';
+
+    echo 'Agora o tipo de dados é: ' . gettype($var);
+    echo '<br>';
+
+    $var = 123;
+    echo "Agora o tipo de dados é: " . gettype($var);
+
+
+?> 
