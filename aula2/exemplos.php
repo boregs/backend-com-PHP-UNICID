7b1b5bd4ecb042347f7390acd6b2ebc83fcf57fb
RAFAEL BORGES DA SILVA <rafaelsilva007@cs.unicid.edu.br>
2025-09-01 21:35:51 -0300

    Arquivos aula 2

---
+++ aula2/exemplos.php
@@ -0,0 +1,21 @@
+<?php 
+    $x = 1;
+    $y = 5;
+
+    // if else basico 
+    if ($x < $y){
+        echo "<h2 align='center'> teste </h2>"; // h2 = aumenta o tamanho do texto | align='center' = alinha ao centro
+                                                // funçoes do html para formatar texto
+    }
+
+    // print em PHP
+
+    print("Frase impressa usando o comando print");
+
+    echo"<br>";
+
+    // arrays em PHP
+
+    $vetor = array('isso', 'é', 'um', 'array');
+    var_dump($vetor);
+?>
---
+++ aula2/site_com_backend/recebedados.php
@@ -0,0 +1,7 @@
+<?php 
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+
+echo "Nome: $nome <br><br>";
+echo "Email: $email";
+?>
