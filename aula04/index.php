8240f148b6abd6b597a52d640c325d320db42f2c
RAFAEL BORGES DA SILVA <rafaelsilva007@cs.unicid.edu.br>
2025-09-15 20:23:01 -0300

    Aula 4 - Exemplo de IF e ELSE

--- aula04/index.php
+++ aula04/index.php
@@ -1,11 +1,24 @@
 <?php 
 // supondo que a variavel $mes recebeu um dado via front-end
 $mes = 12;
+$sexo = 'f';
+
+# verificando se o mes é valido
 if ($mes == 1 || $mes == 12 && $mes < 13){
     echo "Condição verdadeira". '<br>';
-    print("Estamos no mês: " . $mes);
+    print("Estamos no mês: " . $mes . '<br>');
 }
 else{
     echo "Condição falsa";
+}
+
+# verificando se o sexo é valido
+if ($sexo != 'f' && $sexo !='F'&& $sexo != 'm' && $sexo !='M'){
+    print("Sexo Invalido.". '<br>');
+    print("Escolha um sexo valido.");
+}
+else{
+    print("--------- Verificando ---------" . '<br>');
+    print("Sexo Valido! Bem Vindo!" . '<br>');
 }
 ?>
