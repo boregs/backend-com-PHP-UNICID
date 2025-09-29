88b7ca9afee9289ba691ce92fbcbe189232f7ecd
RAFAEL BORGES DA SILVA <rafaelsilva007@cs.unicid.edu.br>
2025-09-29 20:51:06 -0300

    Aula 6 - Arrays

---
+++ aula6/index.php
@@ -0,0 +1,19 @@
+<?php 
+
+//exemplos de array simples
+
+$valor = [2,6,3,6,8,4,7,3,];
+$nomes = ["Xereca","Pau","Agua","aiai","uiui","P diddy","Lebron james","Aspas","sla cara"];
+
+echo 'O 3o numero do array é:';
+print($valor[2]);
+
+echo '<br>';
+
+echo 'O 3o numero do array é:';
+print($valor[5]);
+
+
+echo "um dos nomes é: ";
+print($nomes[4]);
+?>
