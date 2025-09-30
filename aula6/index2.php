1e57944144d7877255aed9266cb4d3b53e764c45
RAFAEL BORGES DA SILVA <rafaelsilva007@cs.unicid.edu.br>
2025-09-29 21:41:16 -0300

    Aula 6 - For Each

---
+++ aula6/index2.php
@@ -0,0 +1,18 @@
+<?php
+// FOR EACH
+
+//dicionario em php
+$notas = [
+    'n1' => 8,
+    'n2' => 5,
+    'n3' => 7,
+    'n4' => 9,
+    'n5' => 4
+];
+foreach($notas as $chave => $valor){
+    echo "Nota - $chave: $valor <br>"
+}
+
+
+
+?>
