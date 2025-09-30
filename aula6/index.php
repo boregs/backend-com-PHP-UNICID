551ed0fa041799cee14b854c55f9b2f4af4fb626
RAFAEL BORGES DA SILVA <rafaelsilva007@cs.unicid.edu.br>
2025-09-29 21:33:46 -0300

    Aula 6 - Arrays

--- aula6/index.php
+++ aula6/index.php
@@ -1,19 +1,43 @@
 <?php 
 
-//exemplos de array simples
+//exemplos manipulação de array simples
 
-$valor = [2,6,3,6,8,4,7,3,];
-$nomes = ["Xereca","Pau","Agua","aiai","uiui","P diddy","Lebron james","Aspas","sla cara"];
+$var = $_POST['seleção_cor'];
+$valor = [2,6,3,6,8,4,7,3,4];
+$nomes = ["Rafael","Kaiky","Anna","João","Pedro","Vito","José","Mario","Luigi","André"];
 
-echo 'O 3o numero do array é:';
+$tamanho_valor = sizeof($valor);
+$tamanho_nomes = sizeof($nomes);
+
+echo '<br>Tamanho do Array $valor = '. $tamanho_valor;
+echo '<br>Tamanho do Array $nomes = '. $tamanho_nomes;
+
+echo '<br>O dado no index 2 é: ';
 print($valor[2]);
-
 echo '<br>';
 
-echo 'O 3o numero do array é:';
+echo 'O dado no index 5 é: ';
 print($valor[5]);
+echo '<br>';
 
+echo "Um dos nomes: ";
+print($nomes[5]);
+echo '<br>';
 
-echo "um dos nomes é: ";
-print($nomes[4]);
+echo 'Itens do Array valor';
+echo '<br>';
+
+$i = 0;
+//For Loop
+for($i = 0;$i < $tamanho_valor; $i++){
+    print("$valor[$i]");
+    echo '<br>';
+}
+
+// Forma Do While
+do{
+    print("$nomes[$i]");
+    echo '<br>';
+    $i++;
+}while($i < $tamanho_nomes);
 ?>
